e9c725b244328a279a60d728ec9673e38f5cc09d
Naman-Bhrgv <62867828+Naman-Bhrgv@users.noreply.github.com>
2023-10-08 11:41:49 -0400

    Added Data Mining file: Data_Mining_class.py

---
+++ Data_Mining_class.py
@@ -0,0 +1,105 @@
+import requests # This library is for the request to HTTP
+import os # Interacting with operating system
+import json
+import configparser
+import csv
+import nltk 
+from nltk.sentiment import SentimentIntensityAnalyzer
+import textblob
+from textblob import TextBlob
+import shutil
+
+class Data_Mining():
+
+    def set_api():
+
+        config = configparser.ConfigParser()
+        config.read(r'config.ini')
+
+        api_key = config['twitter']['api_key']
+        api_key_secret = config['twitter']['api_key_secret']
+
+        access_token = config['twitter']['access_token']
+        access_token_secret = config['twitter']['access_token_secret']
+        bearer_token = config['twitter']['bearer_token']
+
+    def query():
+
+        search_url = "https://api.twitter.com/2/tweets/search/recent"
+
+        # Optional params: start_time, end_time, since_id, until_id, max_results, next_token,
+        # expansions, tweet.fields, media.fields, poll.fields, place.fields, user.fields
+
+        # This place id represents for the United States
+        place_id = "96683cc9126741d1"
+        query = '"nuclear power" future  lang:en -is:retweet'
+
+        query_params = {
+            'query': query,
+            'max_results' : 100,
+            'expansions' : 'attachments.poll_ids,attachments.media_keys,author_id,geo.place_id,in_reply_to_user_id,referenced_tweets.id,entities.mentions.username,referenced_tweets.id.author_id,edit_history_tweet_ids',
+            'tweet.fields': 'attachments,author_id,context_annotations,conversation_id,created_at,edit_controls,edit_history_tweet_ids,entities,geo,id,in_reply_to_user_id,lang,note_tweet,possibly_sensitive,public_metrics,referenced_tweets,reply_settings,source,text,withheld'
+        }
+
+        return query_params
+
+
+    def bearer_oauth(r):
+        """
+        Method required by bearer token authentication.
+        """
+        r.headers["Authorization"] = f"Bearer {bearer_token}"
+        r.headers["User-Agent"] = "v2RecentSearchPython"
+        return r
+    
+    def connect_to_endpoint(url, params):
+        response = requests.get(url, auth=bearer_oauth, params=params)
+        print(response.status_code)
+        if response.status_code != 200: # 200 means that the request is successful.
+            raise Exception(response.status_code, response.text)
+        return response.json()
+    
+    def json_to_csv(json_file, csv_file):
+        # Open and read the json file, which came from the API.
+        with open(json_file, 'r') as file:
+            tweets = json.load(file)
+
+        # Move the information in the json file to the csv file.
+        with open(csv_file, 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            # If this is the first row,
+            if file.tell() == 0 :
+                # Head of the csv.file
+                writer.writerow(['text','Keywords','Author ID','Created At'])
+
+            for tweet in tweets['data']:
+                text = tweet['text']
+                author_id = tweet['author_id']
+                created_at = tweet['created_at']
+
+                writer.writerow([tweets, query, author_id, created_at])
+        print("CSV file saved successfully.")
+
+
+dm=Data_Mining()
+
+qp=dm.query()
+
+search_url = "https://api.twitter.com/2/tweets/search/recent"
+
+json_response = dm.connect_to_endpoint(search_url, qp)
+    
+# Files to be stored
+json_file ='tweets.json'
+csv_file = 'Tweets.csv' # csv.file to be stored
+output_directory = r'/home/ohwang/Desktop/NLP_Github/Data_labeling'
+    
+# Building a JSON file usin JSON response.
+with open(json_file, 'w') as file :
+    json.dump(json_response, file)
+
+# Converting json to csv file.
+csv_path = os.path.join(output_directory, csv_file)
+dm.json_to_csv('tweets.json',csv_path)
+print("csv file is saved successfully as \"Tweets.csv\". ")
+print(f"Saved directory : {csv_path}")
